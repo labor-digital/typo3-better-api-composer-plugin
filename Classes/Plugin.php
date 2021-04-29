bbbc42a18f596f25d65d465287404319fc43f844
Martin Neundorfer <m.neundorfer@labor.digital>
2021-04-29 22:23:10 +0200

    style: update copyright

--- Classes/Plugin.php
+++ Classes/Plugin.php
@@ -1,4 +1,22 @@
 <?php
+/*
+ * Copyright 2021 LABOR.digital
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Last modified: 2021.04.19 at 21:23
+ */
+
 declare(strict_types=1);
 /**
  * Copyright 2019 LABOR.digital
@@ -31,61 +49,61 @@ use RuntimeException;
 
 class Plugin implements PluginInterface, EventSubscriberInterface
 {
-
+    
     /**
      * The event we dispatch to execute our custom php script
      */
     public const EVENT_FIND_VAR_DIR = 'betterApi__install--findVarDir';
-
+    
     /**
      * The path to the include template file, relative to __DIR__
      */
     protected const INCLUDE_TEMPLATE_FILE = '/../Resource/autoloadInclude.tpl.php';
-
+    
     /**
      * The path to the rendered include file relative to $vendorPath
      */
     protected const INCLUDE_FILE = '/labor-digital/betterApiAutoloadInclude.php';
-
+    
     /**
      * @var Composer
      */
     protected $composer;
-
+    
     /**
      * @var IOInterface
      */
     protected $io;
-
+    
     /**
      * The absolute path to the vendor directory
      *
      * @var string
      */
     protected $vendorPath;
-
+    
     /**
      * @inheritDoc
      */
     public static function getSubscribedEvents()
     {
         return [
-            'pre-autoload-dump'  => ['onPreAutoloadDump', -500],
+            'pre-autoload-dump' => ['onPreAutoloadDump', -500],
             'post-autoload-dump' => ['onPostAutoloadDump', -500],
         ];
     }
-
+    
     /**
      * @inheritDoc
      */
     public function activate(Composer $composer, IOInterface $io)
     {
-        $this->composer   = $composer;
-        $this->io         = $io;
-        $config           = $this->composer->getConfig();
+        $this->composer = $composer;
+        $this->io = $io;
+        $config = $this->composer->getConfig();
         $this->vendorPath = Path::normalize(realpath($config->get('vendor-dir')));
     }
-
+    
     /**
      * Register our autoload file to the main package
      * We will fill the file with contents, after the auto-load definition has been dumped
@@ -94,7 +112,7 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     {
         $this->registerAutoloadFile();
     }
-
+    
     /**
      * Builds the content
      */
@@ -102,7 +120,7 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     {
         $this->buildAutoloadFile();
     }
-
+    
     /**
      * Loads the template of the autoload file, injects the required placeholders
      * and dumps it on the final location
@@ -110,37 +128,37 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     protected function buildAutoloadFile(): void
     {
         // Make var path relative to the vendor path
-        $varPath         = $this->findVarPath();
+        $varPath = $this->findVarPath();
         $varPathRelative = Path::makeRelative($varPath, $this->vendorPath);
-
+        
         // Load and build the template
         $tpl = Fs::readFile(__DIR__ . static::INCLUDE_TEMPLATE_FILE);
         $tpl = str_replace('{{varPath}}', $varPathRelative, $tpl);
-
+        
         // Write the template into the output file
         $filePath = $this->vendorPath . static::INCLUDE_FILE;
         Fs::writeFile($filePath, $tpl);
         $this->io->write('<info>Better API - Composer Plugin: Built dynamic autoloading file at: $filePath</info>',
             true, IOInterface::VERBOSE);
-
+        
     }
-
+    
     /**
      * Registers the autoload file as last possible include in the composer autoloader
      */
     protected function registerAutoloadFile(): void
     {
         // Register the file in the root package
-        $rootPackage        = $this->composer->getPackage();
+        $rootPackage = $this->composer->getPackage();
         $autoloadDefinition = $rootPackage->getAutoload();
-        $includeFile        = $this->vendorPath . static::INCLUDE_FILE;
+        $includeFile = $this->vendorPath . static::INCLUDE_FILE;
         Fs::touch($includeFile);
         $autoloadDefinition['files'][] = $includeFile;
         $rootPackage->setAutoload($autoloadDefinition);
         $this->io->write('<info>Better API - Composer Plugin: Injected dynamic autoload file into root package</info>',
             true, IOInterface::VERBOSE);
     }
-
+    
     /**
      * Finds the var directory by calling the varDirFinder.php we ship in the bin directory.
      * It will call the TYPO3 SystemEnvironmentBuilder and send the directory back to us.
@@ -155,18 +173,18 @@ class Plugin implements PluginInterface, EventSubscriberInterface
         if (! empty($extra['T3BA']['isDev'])) {
             $dummyDir = sys_get_temp_dir() . '/T3BA_var';
             Fs::mkdir($dummyDir);
-
+            
             return $dummyDir . '/';
         }
-
+        
         // Dispatch the event to call our custom script
         $temporaryFilePath = sys_get_temp_dir() . '/typo3-var-dir.txt';
-        $dispatcher        = $this->composer->getEventDispatcher();
-        $autoloadFilePath  = $this->vendorPath . '/autoload.php';
+        $dispatcher = $this->composer->getEventDispatcher();
+        $autoloadFilePath = $this->vendorPath . '/autoload.php';
         $dispatcher->addListener(static::EVENT_FIND_VAR_DIR,
             '@php ' . __DIR__ . '/../bin/varDirFinder.php "' . $autoloadFilePath . '" "' . $temporaryFilePath . '"');
         $dispatcher->dispatch(static::EVENT_FIND_VAR_DIR);
-
+        
         // Load the var directory
         if (! fs::isReadable($temporaryFilePath)) {
             $message = 'Could not read the interchange file at: $temporaryFilePath';
@@ -177,8 +195,8 @@ class Plugin implements PluginInterface, EventSubscriberInterface
         Fs::remove($temporaryFilePath);
         $varPath = rtrim($varPath, '/\\') . '/';
         $varPath = str_replace('\\', '/', $varPath);
-
-
+        
+        
         // Create the directory if required
         try {
             Fs::mkdir($varPath);
@@ -190,7 +208,7 @@ class Plugin implements PluginInterface, EventSubscriberInterface
             $this->io->write('<error>Better API - Composer Plugin: $message</error>');
             throw new RuntimeException($message, null, $e);
         }
-
+        
         // Done
         return $varPath;
     }
--- bin/varDirFinder.php
+++ bin/varDirFinder.php
@@ -1,6 +1,6 @@
 <?php
-/**
- * Copyright 2019 LABOR.digital
+/*
+ * Copyright 2021 LABOR.digital
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Last modified: 2019.09.24 at 11:47
+ * Last modified: 2021.04.29 at 22:21
  */
 
 // Exit early if php requirement is not satisfied.
@@ -22,25 +22,29 @@ use Neunerlei\FileSystem\Fs;
 use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Core\SystemEnvironmentBuilder;
 
-if (PHP_VERSION_ID < 70200) die('This version of TYPO3 CMS requires PHP 7.2 or above');
+if (PHP_VERSION_ID < 70200) {
+    die('This version of TYPO3 CMS requires PHP 7.2 or above');
+}
 
 // Ignore non cli calls
-if (php_sapi_name() !== 'cli') die("This is only for CLI execution!");
+if (php_sapi_name() !== 'cli') {
+    die("This is only for CLI execution!");
+}
 
 // Get arguments
 $autoLoadFile = $argv[1];
-if (empty($autoLoadFile) || !file_exists($autoLoadFile)) {
-	echo "Failed to find \"var\" directory: Missing autoload file declaration!" . PHP_EOL;
-	exit(1);
+if (empty($autoLoadFile) || ! file_exists($autoLoadFile)) {
+    echo "Failed to find \"var\" directory: Missing autoload file declaration!" . PHP_EOL;
+    exit(1);
 }
 $tempFile = $argv[2];
 if (empty($tempFile)) {
-	echo "Failed to find \"var\" directory: Missing temporary file declaration!" . PHP_EOL;
-	exit(1);
+    echo "Failed to find \"var\" directory: Missing temporary file declaration!" . PHP_EOL;
+    exit(1);
 }
 
 // Mark this request
-define("BETTER_API_COMPOSER_PLUGIN_VAR_DIR_FINDER", TRUE);
+define("BETTER_API_COMPOSER_PLUGIN_VAR_DIR_FINDER", true);
 
 // Include the autoloader
 include $autoLoadFile;
@@ -50,9 +54,9 @@ $_SERVER['argv'][0] = "/index.php";
 
 // Build the environment and store the var path
 try {
-	SystemEnvironmentBuilder::run(0, SystemEnvironmentBuilder::REQUESTTYPE_CLI);
-	Fs::writeFile($tempFile, Environment::getVarPath());
+    SystemEnvironmentBuilder::run(0, SystemEnvironmentBuilder::REQUESTTYPE_CLI);
+    Fs::writeFile($tempFile, Environment::getVarPath());
 } catch (Exception $e) {
-	echo $e->getMessage() . PHP_EOL;
-	exit(1);
+    echo $e->getMessage() . PHP_EOL;
+    exit(1);
 }
